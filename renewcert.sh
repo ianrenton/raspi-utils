30f8b170258fed2bd6ff28d277ce5d8ee585a9f7
Ian Renton <ian@ianrenton.com>
2020-10-04 08:37:58 +0100

    Fix command order in renewcert

--- renewcert.sh
+++ renewcert.sh
@@ -1,4 +1,4 @@
-systemctl stop lighttpd
 certbot certonly --webroot -w /var/www/html -d mciserver.zapto.org
+systemctl stop lighttpd
 cat /etc/letsencrypt/live/mciserver.zapto.org/privkey.pem /etc/letsencrypt/live/mciserver.zapto.org/cert.pem > /etc/letsencrypt/live/mciserver.zapto.org/web.pem
 systemctl start lighttpd
